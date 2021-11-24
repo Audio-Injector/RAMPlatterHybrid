919958824b464d32b1cd9f0ba9ded88841e96042
Matt Flax <flatmax@flatmax.org>
2021-11-25 09:47:57 +1100

    Updated swig

--- software/test/RunTest.js
+++ software/test/RunTest.js
@@ -1,3 +1,6 @@
 #! /usr/bin/env node
 var libSwigCNodejs = require('../swig/.libs/libSwigCNodejs');
+console.log('constructing Audio<int>')
+let audio = new libSwigCNodejs.intAudio;
+console.log('constructing PlatterAudio<int, 48000>')
 let platterAudio = new libSwigCNodejs.intPlatterAudio48k;
